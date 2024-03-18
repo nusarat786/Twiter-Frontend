59916c8627521c4e1305b797476799e4f029716e
nusarat786 <95936097+nusarat786@users.noreply.github.com>
2024-03-19 02:54:09 +0530

    5

--- src/App.js
+++ src/App.js
@@ -49,11 +49,78 @@ function App() {
       <br></br>
       <br></br>
       <Routes>
-        {console.log(islogined)}
+        
+      <Router>
 
-          <Route path='/login' element={<Login islogined={islogined}/>} />
-          <Route path='/register' element={<Register islogined={islogined} />} />
-          
+<div className='m-10 '>
+  <MainLayout islogined={islogined}/>
+</div>
+<br></br>
+<br></br>
+<br></br>
+<Routes>
+  {console.log(islogined)}
+
+    <Route path='/login' element={<Login islogined={islogined}/>} />
+    <Route path='/register' element={<Register islogined={islogined} />} />
+    
+
+    {islogined ? (
+                <>
+      
+                <Route path='/tweet' element={<Tweet islogined={islogined}/>} />
+                <Route path='/update' element={<Update id={getCookie("id")} islogined={islogined}/>}/>
+                
+                
+                <Route path='/mytweet' element={<TweetByUserId islogined={islogined}/>} />
+                <Route path='/gettweet' element={<TweetCardList   islogined={islogined}/>} />
+                <Route path='/tweet-card' element={<TweetCard  islogined={islogined}/>} />
+                <Route path='/gettweet2' element={<TweetCardList2  islogined={islogined}/>} />
+                <Route path='/profile' element={<ProfileCard  islogined={islogined}/>} />
+                <Route path='/single-tweet/:tid' element={<TweetCardList4  islogined={islogined}/>} />
+                <Route path='/getComments/:tid' element={<Comments  islogined={islogined}/>} />
+                <Route path='/getComments2/:tid' element={<Comments2  islogined={islogined}/>} />
+                
+              
+                {/*  slider */}
+                <Route path='/all-records' element={<TwitterMenuSlider  islogined={islogined}/>} />
+                <Route path='/all-records/all-tweets' element={<TwitterMenuSlider  islogined={islogined} comp={"component1"}/>} />
+                <Route path='/all-records/like-tweets' element={<TwitterMenuSlider  islogined={islogined} comp={"component2"}/>} />
+                <Route path='/all-records/bookmark-tweets' element={<TwitterMenuSlider  islogined={islogined} comp={"component3"}/>} />
+                <Route path='/all-records/replies' element={<TwitterMenuSlider  islogined={islogined} comp={"component4"}/>} />
+                <Route path='/my-details' element={<ProfileCard  islogined={islogined} id={getCookie('id')}/>} />
+                
+                
+                {/* Trndings */}
+                <Route path='/trending/' element={<HomeGet  islogined={islogined} />} />
+                <Route path='/trending/trends' element={<HomeGet  islogined={islogined} comp ={'component2'}/>} />
+                <Route path='/trending/feed' element={<HomeGet  islogined={islogined}  comp ={'component1'}/>} />
+                <Route path='/hashTagScearch/:hashtag' element={<GetHastagsTweet  islogined={islogined}/>} />
+                
+    
+    
+                <Route path='/forget-password' element={<ForgetPassword  islogined={islogined}/>} />
+    
+                <Route path='/scearchByName' element={<ScaerchUser  islogined={islogined}  comp ={'component1'}/>} />
+                <Route path='/scearchByUserName' element={<ScaerchUser  islogined={islogined}  comp ={'component2'}/>} />
+                <Route path='/scearchTweet' element={<ScaerchUser  islogined={islogined}  comp ={'component3'}/>} />
+                
+                
+                <Route path='/user-profile/:id' element={<TwitterMenuSliderUser  islogined={islogined} />} />
+                <Route path='*' element={<HomeGet islogined={islogined}/>} />
+                
+              </>
+    ) : (
+        // Your else content here, for example, a redirect to login page
+        <Route path='*' element={<Login islogined={islogined}/>} />
+    )}
+
+    
+    
+</Routes>
+
+</Router>
+
 
           
 
